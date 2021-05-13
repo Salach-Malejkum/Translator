b6a0a8f72d3cde0adfff9992793bd4cbf3694ed4
Mateusz Salach <mateuszsalach00@gmail.com>
2021-05-13 19:20:34 +0200

    final version of gui with icons

--- GUI.py
+++ GUI.py
@@ -1,60 +1,286 @@
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import *
-from translate import translate_google_cloud_api
-import app
+from PyQt5 import QtCore, QtGui, QtWidgets
+import data_base
 
 
 class Translator(object):
-    def setupUi(self, MainWindow, lang_list):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(874, 489)
-        self.centralwidget = QWidget(MainWindow)
+    __db = data_base.DataBase()
+    __language_dict = __db.get_languages()
+
+    def setup_ui(self, main_window):
+        main_window.setObjectName("MainWindow")
+        main_window.resize(871, 444)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        size_policy.setHeightForWidth(main_window.sizePolicy().hasHeightForWidth())
+        main_window.setSizePolicy(size_policy)
+        main_window.setMinimumSize(QtCore.QSize(871, 444))
+        main_window.setMaximumSize(QtCore.QSize(871, 444))
+        main_window.setStyleSheet("background-color: rgb(103, 103, 103);")
+        self.centralwidget = QtWidgets.QWidget(main_window)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.button_trans = QPushButton(self.centralwidget)
-        self.button_trans.setGeometry(QtCore.QRect(290, 270, 75, 23))
-        self.button_trans.setObjectName("translate_button")
-        self.button_trans.clicked.connect(lambda: self.action_translate())
+        self.translate_button = QtWidgets.QPushButton(self.centralwidget)
+        self.translate_button.setGeometry(QtCore.QRect(290, 270, 71, 23))
+        self.translate_button.setStyleSheet("QPushButton{\n"
+                                            "background-color:#00a82d;\n"
+                                            "    border-radius:10px;\n"
+                                            "    border:1px solid #18ab29;\n"
+                                            "    display:inline-block;\n"
+                                            "    cursor:pointer;\n"
+                                            "    color:#ffffff;\n"
+                                            "    font-family:Arial;\n"
+                                            "    font-size:11px;\n"
+                                            "    text-decoration:none;\n"
+                                            "    text-shadow:0px 1px 0px #2f6627;\n"
+                                            "}\n"
+                                            "QPushButton:pressed {\n"
+                                            "    background-color:#15d648;\n"
+                                            "}")
+        self.translate_button.setObjectName("translate_button")
 
-        self.languages_in = QComboBox(self.centralwidget)
+        self.languages_in = QtWidgets.QComboBox(self.centralwidget)
         self.languages_in.setGeometry(QtCore.QRect(70, 100, 131, 22))
+        self.languages_in.setStyleSheet("QComboBox{\n"
+                                        "    border:1px solid #18ab29;\n"
+                                        "    border-radius: 10px;\n"
+                                        "    background: #007509;\n"
+                                        "    display:inline-block;\n"
+                                        "    cursor:pointer;\n"
+                                        "    color:#ffffff;\n"
+                                        "    font-family:Arial;\n"
+                                        "    font-size:11px;\n"
+                                        "    text-decoration:none;\n"
+                                        "    text-shadow:0px 1px 0px #2f6627;\n"
+                                        "}\n"
+                                        "\n"
+                                        "QComboBox::drop-down {\n"
+                                        "    border-left-width: 1px;\n"
+                                        "    border-left-color:#18ab29;\n"
+                                        "    border-left-style: solid; /* just a single line */\n"
+                                        "    border-top-right-radius: 10px; /* same radius as the QComboBox */\n"
+                                        "    border-bottom-right-radius: 10px;\n"
+                                        "}\n"
+                                        "\n"
+                                        "QComboBox::on {\n"
+                                        "    background-color:#00a82d;\n"
+                                        "    display:inline-block;\n"
+                                        "    cursor:pointer;\n"
+                                        "    color:#ffffff;\n"
+                                        "    font-family:Arial;\n"
+                                        "    font-size:11px;\n"
+                                        "    text-decoration:none;\n"
+                                        "    text-shadow:0px 1px 0px #2f6627;\n"
+                                        "}\n"
+                                        "QComboBox::down-arrow{\n"
+                                        "    image:url(./icons/blackarrow.png);\n"
+                                        "    width: 14px;\n"
+                                        "    height: 14px;\n"
+                                        "}\n"
+                                        "QComboBox::down-arrow:on{\n"
+                                        "    image:url(./icons/whitearrow.png);\n"
+                                        "    width: 14px;\n"
+                                        "    height: 14px;\n"
+                                        "}\n"
+                                        "QComboBox QAbstractItemView {\n"
+                                        "    selection-background-color:#00a82d;\n"
+                                        "    border:1px solid #18ab29;\n"
+                                        "    border-radius: 10px;\n"
+                                        "}\n"
+                                        "QListView {\n"
+                                        "    background: #007509;\n"
+                                        "    color: #ffffff\n"
+                                        "}")
         self.languages_in.setObjectName("languages_in")
-        self.languages_in.addItems(lang_list)
+        self.languages_in.addItems(self.__language_dict.keys())
 
-        self.languages_out = QComboBox(self.centralwidget)
+        self.languages_out = QtWidgets.QComboBox(self.centralwidget)
         self.languages_out.setGeometry(QtCore.QRect(510, 100, 131, 22))
+        self.languages_out.setStyleSheet("QComboBox{\n"
+                                         "    border:1px solid #18ab29;\n"
+                                         "    border-radius: 10px;\n"
+                                         "    background: #007509;\n"
+                                         "    display:inline-block;\n"
+                                         "    cursor:pointer;\n"
+                                         "    color:#ffffff;\n"
+                                         "    font-family:Arial;\n"
+                                         "    font-size:11px;\n"
+                                         "    text-decoration:none;\n"
+                                         "    text-shadow:0px 1px 0px #2f6627;\n"
+                                         "}\n"
+                                         "\n"
+                                         "QComboBox::drop-down {\n"
+                                         "    border-left-width: 1px;\n"
+                                         "    border-left-color:#18ab29;\n"
+                                         "    border-left-style: solid; /* just a single line */\n"
+                                         "    border-top-right-radius: 10px; /* same radius as the QComboBox */\n"
+                                         "    border-bottom-right-radius: 10px;\n"
+                                         "}\n"
+                                         "\n"
+                                         "QComboBox::on {\n"
+                                         "    background-color:#00a82d;\n"
+                                         "    display:inline-block;\n"
+                                         "    cursor:pointer;\n"
+                                         "    color:#ffffff;\n"
+                                         "    font-family:Arial;\n"
+                                         "    font-size:11px;\n"
+                                         "    text-decoration:none;\n"
+                                         "    text-shadow:0px 1px 0px #2f6627;\n"
+                                         "}\n"
+                                         "QComboBox::down-arrow{\n"
+                                         "    image:url(./icons/blackarrow.png);\n"
+                                         "    width: 14px;\n"
+                                         "    height: 14px;\n"
+                                         "}\n"
+                                         "QComboBox::down-arrow:on{\n"
+                                         "    image:url(./icons/whitearrow.png);\n"
+                                         "    width: 14px;\n"
+                                         "    height: 14px;\n"
+                                         "}\n"
+                                         "QComboBox QAbstractItemView {\n"
+                                         "    selection-background-color:#00a82d;\n"
+                                         "    border:1px solid #18ab29;\n"
+                                         "    border-radius: 10px;\n"
+                                         "}\n"
+                                         "QListView {\n"
+                                         "    background: #007509;\n"
+                                         "    color: #ffffff\n"
+                                         "}")
         self.languages_out.setObjectName("languages_out")
-        self.languages_out.addItems(lang_list)
+        self.languages_out.addItems(self.__language_dict.keys())
 
-        self.text_in = QTextEdit(self.centralwidget)
+        self.text_in = QtWidgets.QTextEdit(self.centralwidget)
         self.text_in.setGeometry(QtCore.QRect(70, 130, 291, 131))
+        self.text_in.setStyleSheet("    border-radius:10px;\n"
+                                   "    border:1px solid #18ab29;\n"
+                                   "    display:inline-block;\n"
+                                   "    cursor:pointer;\n"
+                                   "    color:#ffffff;\n"
+                                   "    font-family:Arial;\n"
+                                   "    font-size:11px;\n"
+                                   "    text-decoration:none;\n"
+                                   "    text-shadow:0px 1px 0px #2f6627;\n"
+                                   "background-color: rgb(165, 165, 165);")
         self.text_in.setObjectName("text_in")
+        # self.text_in.act
 
-        self.text_out = QTextEdit(self.centralwidget)
+        self.text_out = QtWidgets.QTextEdit(self.centralwidget)
         self.text_out.setEnabled(False)
         self.text_out.setGeometry(QtCore.QRect(510, 130, 291, 131))
+        self.text_out.setStyleSheet("    border-radius:10px;\n"
+                                    "    border:1px solid #18ab29;\n"
+                                    "    display:inline-block;\n"
+                                    "    cursor:pointer;\n"
+                                    "    color:#ffffff;\n"
+                                    "    font-family:Arial;\n"
+                                    "    font-size:11px;\n"
+                                    "    text-decoration:none;\n"
+                                    "    text-shadow:0px 1px 0px #2f6627;\n"
+                                    "background-color: rgb(145, 145, 145);")
         self.text_out.setObjectName("text_out")
 
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
+        self.autocomplete = QtWidgets.QLabel(self.centralwidget)
+        self.autocomplete.setGeometry(QtCore.QRect(70, 270, 211, 20))
+        self.autocomplete.setStyleSheet("background-color: rgb(127, 127, 127);\n"
+                                        "    border-radius:10px;\n"
+                                        "    border:1px solid #18ab29;\n"
+                                        "    display:inline-block;\n"
+                                        "    cursor:pointer;\n"
+                                        "    color:#ffffff;\n"
+                                        "    font-family:Arial;\n"
+                                        "    font-size:11px;\n"
+                                        "    text-decoration:none;\n"
+                                        "    text-shadow:0px 1px 0px #2f6627;")
+        self.autocomplete.setObjectName("label")
+
+        self.save_button = QtWidgets.QPushButton(self.centralwidget)
+        self.save_button.setGeometry(QtCore.QRect(400, 160, 75, 23))
+        self.save_button.setStyleSheet("QPushButton{\n"
+                                       "background-color:#00a82d;\n"
+                                       "    border-radius:10px;\n"
+                                       "    border:1px solid #18ab29;\n"
+                                       "    display:inline-block;\n"
+                                       "    cursor:pointer;\n"
+                                       "    color:#ffffff;\n"
+                                       "    font-family:Arial;\n"
+                                       "    font-size:11px;\n"
+                                       "    text-decoration:none;\n"
+                                       "    text-shadow:0px 1px 0px #2f6627;\n"
+                                       "}\n"
+                                       "QPushButton:pressed {\n"
+                                       "    background-color:#15d648;\n"
+                                       "}")
+        self.save_button.setObjectName("save_button")
+
+        self.load_button = QtWidgets.QPushButton(self.centralwidget)
+        self.load_button.setGeometry(QtCore.QRect(400, 210, 75, 23))
+        self.load_button.setStyleSheet("QPushButton{\n"
+                                       "background-color:#00a82d;\n"
+                                       "    border-radius:10px;\n"
+                                       "    border:1px solid #18ab29;\n"
+                                       "    display:inline-block;\n"
+                                       "    cursor:pointer;\n"
+                                       "    color:#ffffff;\n"
+                                       "    font-family:Arial;\n"
+                                       "    font-size:11px;\n"
+                                       "    text-decoration:none;\n"
+                                       "    text-shadow:0px 1px 0px #2f6627;\n"
+                                       "}\n"
+                                       "QPushButton:pressed {\n"
+                                       "    background-color:#15d648;\n"
+                                       "}")
+        self.load_button.setObjectName("load_button")
+
+        self.swap_button = QtWidgets.QPushButton(self.centralwidget)
+        self.swap_button.setGeometry(QtCore.QRect(400, 100, 75, 21))
+        self.swap_button.setStyleSheet("QPushButton{\n"
+                                       "    icon: url(./icons/swap.png);\n"
+                                       "    background-color:#00a82d;\n"
+                                       "    border-radius:10px;\n"
+                                       "    border:1px solid #18ab29;\n"
+                                       "    display:inline-block;\n"
+                                       "    cursor:pointer;\n"
+                                       "}\n"
+                                       "QPushButton:pressed {\n"
+                                       "    background-color:#15d648;\n"
+                                       "}\n"
+                                       "    ")
+        self.swap_button.setText("")
+
+        icon = QtGui.QIcon.fromTheme("swap.png")
+        self.swap_button.setIcon(icon)
+        self.swap_button.setObjectName("swap_button")
+
+        self.clear_button = QtWidgets.QPushButton(self.centralwidget)
+        self.clear_button.setGeometry(QtCore.QRect(330, 140, 21, 23))
+        self.clear_button.setStyleSheet("QPushButton {\n"
+                                        "    border-radius:10px;\n"
+                                        "    background-color: rgb(165, 165, 165);\n"
+                                        "    icon: url(./icons/clear.png);\n"
+                                        "    display:inline-block;\n"
+                                        "    cursor:pointer;\n"
+                                        "}\n"
+                                        "QPushButton::hover{\n"
+                                        "    background-color: rgb(175, 175, 175);\n"
+                                        "}\n"
+                                        "QPushButton:pressed {\n"
+                                        "    background-color: rgb(185, 185, 185);\n"
+                                        "}")
+        self.clear_button.setText("")
+        self.clear_button.setObjectName("clear_button")
+
+        main_window.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(main_window)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        main_window.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslate_ui(main_window)
+        QtCore.QMetaObject.connectSlotsByName(main_window)
 
-    def retranslateUi(self, MainWindow):
+    def retranslate_ui(self, main_window):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.button_trans.setText(_translate("MainWindow", "Translate"))
-
-    def action_translate(self):
-        src = self.languages_in.currentText()
-        dest = self.languages_out.currentText()
-        text = self.text_in.toPlainText()
-        if src == dest:
-            self.text_out.setText(text)
-        else:
-            src, dest = app.App.get_src_dst(src, dest)
-            translation = translate_google_cloud_api(src, dest, text)
-            self.text_out.setText(translation['translatedText'])
+        main_window.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.translate_button.setText(_translate("MainWindow", "Translate"))
+        self.autocomplete.setText(_translate("MainWindow", "To fill press Tab key:"))
+        self.save_button.setText(_translate("MainWindow", "Save"))
+        self.load_button.setText(_translate("MainWindow", "Load"))
